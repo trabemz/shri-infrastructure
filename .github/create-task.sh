2f30d9276e413dd72844d2dc5adbac5cf01fe246
Anastasia Zamkovskaya <trabem.z@gmail.com>
2021-10-31 14:31:23 +0300

    Creating a task in tracker

--- .github/create-task.sh
+++ .github/create-task.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+#git info
+echo "Getting information from git..."
 PreviousAndCurrentGitTag=`git describe --tags \`git rev-list --tags --abbrev=0 --max-count=2\` --abbrev=0`
 PreviousGitTag=`echo $PreviousAndCurrentGitTag | cut -f 2 -d ' '`
 CurrentGitTag=`echo $PreviousAndCurrentGitTag | cut -f 1 -d ' '`
@@ -6,10 +9,64 @@ CurrentGitTag=`echo $PreviousAndCurrentGitTag | cut -f 1 -d ' '`
 echo "Current tag: " $CurrentGitTag
 echo "Previous tag: " $PreviousGitTag
 
-ReleaseDate=`git rev-parse ${CurrentGitTag} | xargs git cat-file -p | awk '/^tagger/ { print "@" $(NF-1) }' | xargs -i date -d "{}" "+%Y-%m-%d" `
-echo "Release date: " $ReleaseDate
-ReleaseCreator=`git rev-parse ${CurrentGitTag} | xargs git cat-file -p | awk '/^tagger/ { print $2 " " $3 " " $4 }'`
-echo "Release author: " $ReleaseCreator
-GitLog=`git log ${PreviousGitTag}..${CurrentGitTag} --pretty=tformat:"%h; author: %cn; date: %cs; subject:%s %n"`
+Release=`git show $CurrentGitTag --pretty=format:"%as" --no-patch | tr -s "\n" " "`
+GitLog=`git log ${PreviousGitTag}..${CurrentGitTag} --pretty=format:"\n* %h (%cs) %s - %cn %ce;" | tr -s "\n" " "`
+echo $GitLog
+echo $Release
+#create task in tracker
+echo "Creating task in tracker..."
+Summary="Release ${CurrentGitTag}"
+echo $Summary
+Description="Release: ${Release}\nChangelog:\n${GitLog}" 
+echo $Description
+Unique="trabemz_shri-infrastructure_${CurrentGitTag}"
+echo $Unique
 
-echo $GitLog 
+DataRaw='{
+        "queue": "TMP",
+        "summary": "'"${Summary}"'",
+        "description": "'"${Description}"'",
+        "unique": "'"${Unique}"'"
+    }'
+
+responseCode=$(curl -o /dev/null -s -w "%{http_code}" --location --request POST 'https://api.tracker.yandex.net/v2/issues/' \
+--header "Authorization: OAuth $OAuth" \
+--header "X-Org-ID: $OrganizationId" \
+--header "Content-Type: application/json" \
+--data-raw "$DataRaw"
+)
+echo $responseCode
+
+if [ "$responseCode" = 409 ]
+then 
+  echo 'The task was created in the tracker earlier.'
+  echo 'Updating task in tracker...'
+
+  #Get task update link
+  IssueUrl=$(curl --location --request POST 'https://api.tracker.yandex.net/v2/issues/_search' \
+  --header "Authorization: OAuth $OAuth" \
+  --header "X-Org-ID: $OrganizationId" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "filter": {
+      "unique": "'"${Unique}"'"
+    }
+  }' | awk -F '"self":"' '{ print $2 }' | awk -F '","' '{ print $1 }'
+  )
+  echo $IssueUrl
+
+  #Update task
+  responseCodeUpdate=$(curl -o /dev/null -s -w "%{http_code}" --location --request PATCH "$IssueUrl" \
+  --header "Authorization: OAuth $OAuth" \
+  --header "X-Org-ID: $OrganizationId" \
+  --header "Content-Type: application/json" \
+  --data-raw "$DataRaw"
+  )
+
+  echo $responseCodeUpdate
+  if [ "$responseCodeUpdate" = 200 ]
+  then echo "Task updated successfully!" 
+  else echo "Update return code $responseCodeUpdate"
+  fi
+else echo "Task created successfully!" 
+fi
