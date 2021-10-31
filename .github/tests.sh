de9f72e8f0c2aae9f4f58d4c651635d1352638d6
Anastasia Zamkovskaya <trabem.z@gmail.com>
2021-10-31 18:11:10 +0300

    Change to bash

--- .github/tests.sh
+++ .github/tests.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 echo "Running tests..."
 testSuites=$(npx jest --noStackTrace 2>&1)
-
+echo $testSuites
 if [[ $testSuites == *"FAIL"* ]]
 then
   result="!!Test suites were failed! Check your repository.!!"
